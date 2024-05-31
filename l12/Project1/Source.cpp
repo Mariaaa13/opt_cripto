bd3a690fc6f58eb96b96dc838e8b20f8778e3342
Mariaaa13 <mariandriescu13@yahoo.com>
2024-06-01 00:23:10 +0300

    l10/11/12

---
+++ l12/Project1/Source.cpp
@@ -0,0 +1,23 @@
+#include "Header.h"
+
+
+
+
+
+
+
+
+#include"Header.h"
+int main() {
+	Curba E = { 2,2,13 };
+	Punct P = { 8, 6, false };
+	Punct Q = { 8,7,false };
+
+	afisare(E);
+
+	/*af(aduna(E, P, Q));
+	cout << endl;
+	af(aduna(E, P, P))
+	*/
+
+}
